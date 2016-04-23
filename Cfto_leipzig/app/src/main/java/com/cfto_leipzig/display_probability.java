851a10898d041717a4f0ef8bd5176bf4f41204af
Kaihftl <s143224@hft-leipzig.de>
2016-04-23 10:45:37 +0200

    Activities added
    2 Activities added

---
+++ Cfto_leipzig/app/src/main/java/com/cfto_leipzig/display_probability.java
@@ -0,0 +1,16 @@
+package com.cfto_leipzig;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class display_probability extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_probability);
+    }
+}
---
+++ Cfto_leipzig/app/src/main/java/com/cfto_leipzig/mainActivity.java
@@ -0,0 +1,13 @@
+package com.cfto_leipzig;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class mainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+    }
+}
